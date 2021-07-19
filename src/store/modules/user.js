18abbb4e5219d08d353c8cb325e0213ca4179075
you <284908631@qq.com>
2021-07-19 23:39:03 +0800

    feature：能够微信登录成功，但是很多系统代码功能错误

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,5 +1,6 @@
 import { login, logout, getInfo } from '@/api/login'
 import { getToken, setToken, removeToken } from '@/utils/auth'
+import {loginByCode} from "@/api/login";
 
 const user = {
     state: {
@@ -29,7 +30,22 @@ const user = {
     },
 
     actions: {
-        // 登录
+      // 微信登录
+      LoginByCode({ commit }, code) {
+        return new Promise((resolve, reject) => {
+          loginByCode(code)
+            .then(res => {
+              console.log("微信登录res：", res)
+              setToken(res.data.token)
+              commit('SET_TOKEN', res.data.token)
+              resolve()
+            })
+            .catch(error => {
+              reject(error)
+            })
+        })
+      },
+      // 登录
         Login({ commit }, userInfo) {
             const username = userInfo.username.trim()
             const password = userInfo.password
