447624abe9831d766c1e593969cb15bcd15829c2
Peter roe <peterroe@163.com>
2021-09-02 17:25:38 +0800

    fix

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,10 +5,10 @@ import { getToken } from '@/utils/auth'
 import errorCode from '@/utils/errorCode'
 
 axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
-// 创建axios实例
+    // 创建axios实例
 const service = axios.create({
     // axios中请求配置有baseURL选项，表示请求URL公共部分
-    baseURL: 'http://127.0.0.1:8080',
+    baseURL: 'http://192.168.124.8:8080',
     // 超时
     timeout: 10000
 })
@@ -49,7 +49,7 @@ export function tryHideFullScreenLoading() {
 service.interceptors.request.use(
     config => {
         showFullScreenLoading()
-        // 是否需要设置 token
+            // 是否需要设置 token
         const isToken = (config.headers || {}).isToken === false
         if (getToken() && !isToken) {
             config.headers['Authorization'] = 'Bearer ' + getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
@@ -89,20 +89,19 @@ service.interceptors.request.use(
 service.interceptors.response.use(
     res => {
         tryHideFullScreenLoading()
-        // 未设置状态码则默认成功状态
+            // 未设置状态码则默认成功状态
         const code = res.data.code || 200
-        // 获取错误信息
+            // 获取错误信息
         const msg = errorCode[code] || res.data.msg || errorCode['default']
         if (code === 401) {
             MessageBox.confirm(
-                '登录状态已过期，您可以继续留在该页面，或者重新登录',
-                '系统提示',
-                {
-                    confirmButtonText: '重新登录',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }
-            )
+                    '登录状态已过期，您可以继续留在该页面，或者重新登录',
+                    '系统提示', {
+                        confirmButtonText: '重新登录',
+                        cancelButtonText: '取消',
+                        type: 'warning'
+                    }
+                )
                 .then(() => {
                     store.dispatch('LogOut').then(() => {
                         location.href = '/index'
